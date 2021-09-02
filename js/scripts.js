62e0e68e3840d6a36f45d786833127c30c641b3d
ilukin89 <67657429+ilukin89@users.noreply.github.com>
2021-09-02 13:49:16 +0200

    JS arrays added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
-alert('My favourite food is...');
-let favoriteFood ="chocolate";
-document.write (favoriteFood);
+const pokemonList = [
+    {
+    name:"Raticate",
+    height: 0.7,
+    types:"normal"
+},
+
+   {
+    name:"Charmander",
+    height: 0.6,
+    types:"fire"
+
+},
+
+    {
+    name:"Wigglytuff",
+    height: 1,
+    types:["fairy","normal"]
+
+},
+    {
+    name:"Nidoking",
+    height: 1.4,
+    types:["ground","poison"]
+
+},
+    {
+    name:"Victreebel",
+    height: 1.7,
+    types:["grass","poison"]
+
+}];
