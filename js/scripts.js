54e7c4b2f1effcc324f81e23fed8f71f109cd2da
ilukin89 <67657429+ilukin89@users.noreply.github.com>
2021-09-03 18:31:38 +0200

    loop added

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,19 @@ const pokemonList = [{
 
     }
 ];
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write(`${pokemonList[i].name}'s height is ${pokemonList[i].height}<br/>`);
+
+    let pokemonHeight = pokemonList[i].height;
+    let pokemonName = pokemonList[i].name;
+
+    if (pokemonHeight > 1) {
+        document.write(`Wow that's big <br/><br/>`);
+    } else if (pokemonHeight > 0.5 && pokemonHeight < 1) {
+        document.write(`That is average Pokemon <br/><br/>`);
+    } else {
+        document.write(`That is small Pokemon <br/><br/>`);
+    }
+}
