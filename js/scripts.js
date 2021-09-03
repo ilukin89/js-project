3f2da05d798092303904233f1f1f76088edddc5c
ilukin89 <67657429+ilukin89@users.noreply.github.com>
2021-09-04 00:41:58 +0200

    removed unused variables / beautify code

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,45 @@
 const pokemonList = [{
-        name: "Raticate",
-        height: 0.7,
-        types: ["normal"]
-    },
+    name: "Raticate",
+    height: 0.7,
+    types: ["normal"]
+},
 
-    {
-        name: "Charmander",
-        height: 0.6,
-        types: ["fire"]
+{
+    name: "Charmander",
+    height: 0.6,
+    types: ["fire"]
 
-    },
+},
 
-    {
-        name: "Wigglytuff",
-        height: 1,
-        types: ["fairy", "normal"]
+{
+    name: "Wigglytuff",
+    height: 1,
+    types: ["fairy", "normal"]
 
-    },
-    {
-        name: "Nidoking",
-        height: 1.4,
-        types: ["ground", "poison"]
+},
+{
+    name: "Nidoking",
+    height: 1.4,
+    types: ["ground", "poison"]
 
-    },
-    {
-        name: "Victreebel",
-        height: 1.7,
-        types: ["grass", "poison"]
+},
+{
+    name: "Victreebel",
+    height: 1.7,
+    types: ["grass", "poison"]
 
-    }
+}
 ];
 
 
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(`${pokemonList[i].name}'s height is ${pokemonList[i].height}<br/>`);
+document.write(`${pokemonList[i].name}'s height is ${pokemonList[i].height}<br/>`);
 
-    let pokemonHeight = pokemonList[i].height;
-    let pokemonName = pokemonList[i].name;
-
-    if (pokemonHeight > 1) {
-        document.write(`Wow that's big <br/><br/>`);
-    } else if (pokemonHeight > 0.5 && pokemonHeight < 1) {
-        document.write(`That is average Pokemon <br/><br/>`);
-    } else {
-        document.write(`That is small Pokemon <br/><br/>`);
-    }
+if (pokemonList[i].height > 1) {
+    document.write(`Wow that's big <br/><br/>`);
+} else if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1) {
+    document.write(`That is average Pokemon <br/><br/>`);
+} else {
+    document.write(`That is small Pokemon <br/><br/>`);
+}
 }
