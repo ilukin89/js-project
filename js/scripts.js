02bab1f6da0670029b7f9e0a6729318021383fb1
ilukin89 <67657429+ilukin89@users.noreply.github.com>
2021-09-07 15:00:29 +0200

    forEach() applied

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,53 @@
 const pokemonList = [{
     name: "Raticate",
     height: 0.7,
-    types: ["normal"]
-},
+    types: ["normal"],
+  },
 
-{
+  {
     name: "Charmander",
     height: 0.6,
-    types: ["fire"]
+    types: ["fire"],
+  },
 
-},
-
-{
+  {
     name: "Wigglytuff",
     height: 1,
-    types: ["fairy", "normal"]
-
-},
-{
+    types: ["fairy", "normal"],
+  },
+  {
     name: "Nidoking",
     height: 1.4,
-    types: ["ground", "poison"]
-
-},
-{
+    types: ["ground", "poison"],
+  },
+  {
     name: "Victreebel",
     height: 1.7,
-    types: ["grass", "poison"]
-
-}
+    types: ["grass", "poison"],
+  },
+  {
+    name: "Squirtle",
+    height: 0.5,
+    types: ["water"],
+  },
 ];
 
+// for (let i = 0; i < pokemonList.length; i++) {
+//   document.write(
+//     `${pokemonList[i].name}'s height is ${pokemonList[i].height}<br/>`
+//   );
 
-for (let i = 0; i < pokemonList.length; i++) {
-document.write(`${pokemonList[i].name}'s height is ${pokemonList[i].height}<br/>`);
+//   if (pokemonList[i].height > 1) {
+//     document.write(`Wow, that's big! <br/><br/>`);
+//   } else if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1) {
+//     document.write(`That is an average Pokemon <br/><br/>`);
+//   } else {
+//     document.write(`That is a small Pokemon <br/><br/>`);
+//   }
+// }
 
-if (pokemonList[i].height > 1) {
-    document.write(`Wow that's big <br/><br/>`);
-} else if (pokemonList[i].height > 0.5 && pokemonList[i].height < 1) {
-    document.write(`That is average Pokemon <br/><br/>`);
-} else {
-    document.write(`That is small Pokemon <br/><br/>`);
-}
-}
+pokemonList.forEach(function (pokemon) {
+  document.write(
+    `This is ${pokemon.name}. <br> Its height is ${pokemon.height}. <br> It is a ${pokemon.types} type Pokemon.<br><br>`
+  );
+});
